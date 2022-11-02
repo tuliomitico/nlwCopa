7089fffccd9334b5482bc53ae5f65a73fd5f1e1d
tuliomitico <tuliofc2008@hotmail.com>
2022-11-02 16:11:06 -0300

    chore: change to typescript, first screen

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+    return (
+        <Center flex={1} bgColor='gray.900' alignItems={'center'} justifyContent='center'>
+            <Text color='white' fontSize={24} fontFamily={'heading'}>Hello React Native</Text>
+        </Center>
+    )
+}
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,26 @@
+import { GetServerSideProps } from "next";
 
+interface HomeProps {
+  count: number
+}
 
-export default function Home() {
-  return <h1>Hello World!</h1>
+export default function Home(props: HomeProps) {
+  return (
+    <div>
+      <h1>Contagem: {props.count}</h1>
+      <p>Receba, meu pai!</p>
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  console.log(data)
+  return {
+    props: {
+      count: data.count,
+    }
+  }
 }
