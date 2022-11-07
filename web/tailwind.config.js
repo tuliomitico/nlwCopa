24af72db3c31bac53fb06f9ad1be312f2e2b6479
tuliomitico <tuliofc2008@hotmail.com>
2022-11-06 22:26:36 -0300

    chore: web app structure made

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     'src/**/*.tsx',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
+      backgroundImage:{
+        app: 'url(/app-bg.png)'
+      },
+      colors: {
+        gray:{
+          100: '#E1E1E6',
+          300: '#8d8d99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214'
+        },
+        yellow:{
+          500: "#F7DD43",
+          700: "#E5cd3d",
+        },
+        ignite: {
+          500: '#129e57'
+        }
+      }
+    },
   },
   plugins: [],
 }
